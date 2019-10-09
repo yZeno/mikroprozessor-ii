2ca306837e9878b87e259f8cda1f3b6aeb43a942
yZeno <ahyuga.00+git@gmail.com>
2019-10-09 10:35:18 +0200

    Uebung 10 (V2)

--- kapitel1/kapitel1-10.asm
+++ kapitel1/kapitel1-10.asm
@@ -1,15 +1,16 @@
     MOV 	TMOD,#01H
-
+    CLR     TR0 
 start:
-    CLR     TR0         ;1
-
     MOV     TL0,#xxH    ;2
     MOV     TH0,#xxH    ;2
     SETB 	TR0         ;1
-wait:
-    JBC     TF0,exit    ;2x +2
-    SJMP    wait        ;2x
-exit:
+;wait:
+;    JBC     TF0,exit    ;2x +2
+;    SJMP    wait        ;2x
+;exit:
+    JNB 	TF0,$       ;2x
+    CLR     TF0         ;1
+
     CLR     TR0         ;1
 
     CPL     P1.0        ;1
