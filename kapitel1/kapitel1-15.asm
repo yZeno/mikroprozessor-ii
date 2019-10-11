c0910b25a8ad085f6e25906541b73e2154ba4d3f
yZeno <ahyuga.00+git@gmail.com>
2019-10-11 12:42:04 +0200

    Uebung 15

---
+++ kapitel1/kapitel1-15.asm
@@ -0,0 +1,33 @@
+    ORG     0000H
+
+    MOV     A,R0
+    XCH     A,30H
+    MOV     R0,A
+    
+    MOV     A,R1
+    XCH     A,31H
+    MOV     R1,A
+    
+    MOV     A,R2
+    XCH     A,32H
+    MOV     R2,A
+    
+    MOV     A,R3
+    XCH     A,33H
+    MOV     R3,A
+    
+    MOV     A,R4
+    XCH     A,34H
+    MOV     R4,A
+    
+    MOV     A,R5
+    XCH     A,35H
+    MOV     R5,A
+    
+    MOV     A,R6
+    XCH     A,36H
+    MOV     R6,A
+    
+    MOV     A,R7
+    XCH     A,37H
+    MOV     R7,A
