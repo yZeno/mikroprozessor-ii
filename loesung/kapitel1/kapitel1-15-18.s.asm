21d9a7ed81579676c3c66eb31fd8d38880e31f80
yZeno <ahyuga.00+git@gmail.com>
2019-10-22 23:13:46 +0200

    *U18 +U15 wip

--- loesung/kapitel1/kapitel1-15-18.s.asm
+++ loesung/kapitel1/kapitel1-15-18.s.asm
@@ -1,6 +1,33 @@
 ;15)
 ;...
     ORG     8000H
+    ANL     PSW,#11100111B      ;Registerbank 0 auswählen
+    MOV     A,R0                ;1. Tausch
+    XCH     A,30H
+    MOV     R0,A
+    MOV     A,R1                ;2. Tausch
+    XCH     A,31H
+    MOV     R1,A
+    ;...
+    MOV     A,R7                ;8. Tausch
+    XCH     A,37H
+    MOV     R7,A
+    END
+;...
+
+;16)
+;...
+    ORG     8000H
+    MOV     A,60H
+    ADD     A,61H       ;<A> <- <A> + <I:60>>
+    CLR     C           ;carry löschen
+    SUBB    A,#36H
+    ;...
+;...
+
+;18)
+;...
+    ORG     8000H
     MOV	    R0,#30H
     MOV	    DPTR,#8000H
 loop:
